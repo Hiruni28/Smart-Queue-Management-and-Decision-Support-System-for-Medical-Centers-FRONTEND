09c8a6b9132845021645fc199a5c7d1931d4ec49
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-16 21:32:43 +0530

    Updated app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-import AdminLogin from"./pages/AdminLogin";
-import AdminDashboard from"./pages/AdminDashboard";
+import {BrowserRouter,Routes,Route}
+from "react-router-dom";
+
+import AdminLogin from "./pages/AdminLogin";
+
+import AdminDashboard from "./pages/AdminDashboard";
+
 import ManageDoctors from "./pages/ManageDoctors";
+
 import ManageStaff from "./pages/ManageStaff";
+
 import ManageQueue from "./pages/ManageQueue";
+
 import PatientRegister from "./pages/PatientRegister";
+
 import PatientLogin from "./pages/PatientLogin";
+
 import PatientDashboard from "./pages/PatientDashboard";
+
 import PatientProfile from "./pages/PatientProfile";
+
 import PatientAppointments from "./pages/PatientAppointments";
+
+import StaffLogin from "./pages/StaffLogin";
+
+import StaffDashboard from "./pages/StaffDashboard";
 
 function App(){
 
@@ -18,35 +33,72 @@ return(
 
 <Routes>
 
-<Route path="/"element={<AdminLogin/>}/>
+<Route
+path="/"
+element={<AdminLogin/>}
+/>
 
-<Route path="/"element={<PatientLogin/>}/>
+<Route
+path="/dashboard"
+element={<AdminDashboard/>}
+/>
 
-<Route path="/dashboard"element={<AdminDashboard/>}/>
+<Route
+path="/doctors"
+element={<ManageDoctors/>}
+/>
 
-<Route path="/doctors" element={<ManageDoctors/>}/>
+<Route
+path="/staff"
+element={<ManageStaff/>}
+/>
 
-<Route path="/staff" element={<ManageStaff />} />
+<Route
+path="/queue"
+element={<ManageQueue/>}
+/>
 
-<Route path="/queue" element={<ManageQueue />} />
+<Route
+path="/patient-register"
+element={<PatientRegister/>}
+/>
 
-<Route path="/patient-register" element={<PatientRegister/>} />
+<Route
+path="/patient-login"
+element={<PatientLogin/>}
+/>
 
-<Route path="/patient-login" element={<PatientLogin/>} />
+<Route
+path="/patient-dashboard"
+element={<PatientDashboard/>}
+/>
 
-<Route path="/patient-dashboard" element={<PatientDashboard/>} />
+<Route
+path="/patient-profile"
+element={<PatientProfile/>}
+/>
 
-<Route path="/patient-profile" element={<PatientProfile/>} />
+<Route
+path="/appointments"
+element={<PatientAppointments/>}
+/>
 
-<Route path="/appointments" element={<PatientAppointments/>} />
+<Route
+path="/staff-login"
+element={<StaffLogin/>}
+/>
 
-<Route path="/appointments" element={<PatientAppointments/>} />
+<Route
+path="/staff-dashboard"
+element={<StaffDashboard/>}
+/>
 
 </Routes>
 
 </BrowserRouter>
 
 );
+
 }
 
 export default App;
